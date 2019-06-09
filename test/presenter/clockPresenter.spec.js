677374726b460c3216bec00804dd558a096ab8ca
Naveen Anandhan <antiquenaveen@gmail.com>
2019-06-10 00:18:50 +0200

    test: Group error handling test cases into one describe

--- test/presenter/clockPresenter.spec.js
+++ test/presenter/clockPresenter.spec.js
@@ -2,44 +2,47 @@ import { expect } from 'chai';
 import Clock from '../../src/presenter/clockPresenter';
 
 describe('Berlin Clock', () => {
+    let clock;
     beforeEach(() => {
-        const clock = Clock();
+        clock = Clock();
     });
+
+    describe('Error Handling for Invalid Time', () => {
+        it('Should throw an error when an invalid second is passed', () => {
+            let exceptionThrown = false;
     
-    it('Should throw an error when an invalid second is passed', () => {
-        let exceptionThrown = false;
-
-        try{
-            clock.getBerlinClockTime('23:00:70');
-        }catch(exception){
-            exceptionThrown = true;
-        }
-
-        expect(exceptionThrown).to.be.true;
-    });
-
-    it('Should throw an error when an invalid minute is passed', () => {
-        let exceptionThrown = false;
-
-        try{
-            clock.getBerlinClockTime('00:61:00');
-        }catch(exception){
-            exceptionThrown = true;
-        }
-
-        expect(exceptionThrown).to.be.true;
-    });
-
-    it('Should throw an error when an invalid hour is passed', () => {
-        let exceptionThrown = false;
-
-        try{
-            clock.getBerlinClockTime('25:00:00');
-        }catch(exception){
-            exceptionThrown = true;
-        }
-
-        expect(exceptionThrown).to.be.true;
+            try{
+                clock.getBerlinClockTime('23:00:70');
+            }catch(exception){
+                exceptionThrown = true;
+            }
+    
+            expect(exceptionThrown).to.be.true;
+        });
+    
+        it('Should throw an error when an invalid minute is passed', () => {
+            let exceptionThrown = false;
+    
+            try{
+                clock.getBerlinClockTime('00:61:00');
+            }catch(exception){
+                exceptionThrown = true;
+            }
+    
+            expect(exceptionThrown).to.be.true;
+        });
+    
+        it('Should throw an error when an invalid hour is passed', () => {
+            let exceptionThrown = false;
+    
+            try{
+                clock.getBerlinClockTime('25:00:00');
+            }catch(exception){
+                exceptionThrown = true;
+            }
+    
+            expect(exceptionThrown).to.be.true;
+        });
     });
 });  
 
