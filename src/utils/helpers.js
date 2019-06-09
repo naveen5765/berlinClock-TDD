7d0fb03c14e85f09c635535e5c79e35950cf3ad2
Naveen Anandhan <antiquenaveen@gmail.com>
2019-06-10 00:16:37 +0200

    refactor: Group regex validation of seconds, minutes and hours into one

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,12 +6,5 @@ export const prependZero = (number) => {
 };
 
 export const isInValidTime = (time) => {
-    const timeParts = time.split(':');
-    const seconds = timeParts[2];
-    let minutes = timeParts[1];
-    let hours = timeParts[0];
-
-    return !/^([0-5][0-9])?$/.test(seconds) 
-        && !/^([0-5][0-9])?$/.test(minutes)
-        && !/^([0-1]?[0-9]|2[0-4])?$/.test(hours);
+    return !/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(time);
 };
